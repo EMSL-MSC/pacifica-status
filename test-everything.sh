a250ad27c9423e077a7118ba2dc93e1fbd0503b5
dmlb2000 <dmlb2000@gmail.com>
2016-09-15 14:42:41 -0700

    misspelled stuff

--- test-everything.sh
+++ test-everything.sh
@@ -2,4 +2,4 @@
 
 # basic syntax check against all php files
 find application -name '*.php' | xargs -n 1 php -l
-find application -name '*.php' | xargs phpcs --standare=PEAR
+find application -name '*.php' | xargs phpcs --standard=PEAR
