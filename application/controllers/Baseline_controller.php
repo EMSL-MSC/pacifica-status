fc0b18c791434ad27fceb99844e3e57ece90321b
dmlb2000 <dmlb2000@gmail.com>
2016-09-24 11:36:32 -0700

    Add docblocks to appropriate parts of the code. And fix some of the php code sniffer complaints.

--- application/controllers/Baseline_controller.php
+++ application/controllers/Baseline_controller.php
@@ -1,7 +1,24 @@
 <?php
+/**
+ * Controllers Baseline
+ *
+ * PHP Version 5
+ *
+ * @category Controllers
+ * @package  Baseline
+ * @author   Ken Auberry  <Kenneth.Auberry@pnnl.gov>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://github.com/EMSL-MSC/pacifica-upload-status
+ */
 
+/**
+ * Baseline controller class
+ */
 class Baseline_controller extends CI_Controller
 {
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         date_default_timezone_set('America/Los_Angeles');
@@ -17,26 +34,40 @@ class Baseline_controller extends CI_Controller
 
         $this->benchmark->mark('get_user_details_start');
         $user_info = get_user_details_myemsl($this->user_id);
-        $this->username = $user_info['first_name'] != null ? $user_info['first_name'] : 'Anonymous Stranger';
+        if($user_info['first_name'] != null) {
+            $this->username = $user_info['first_name'];
+        } else {
+            'Anonymous Stranger';
+        }
         $this->fullname = "{$this->username} {$user_info['last_name']}";
-        $this->is_emsl_staff = $user_info['emsl_employee'] == 'Y' ? true : false;
-        // $this->is_emsl_staff = false;
+        $this->is_emsl_staff = $user_info['emsl_employee'] == 'Y' ? TRUE : FALSE;
         $this->site_color = $this->config->item('site_color');
 
         $this->email = $user_info['email_address'];
         $user_info['full_name'] = $this->fullname;
-        $user_info['network_id'] = !empty($user_info['network_id']) ? $user_info['network_id'] : 'unknown';
-        $current_path_info = isset($_SERVER['PATH_INFO']) ? ltrim($_SERVER['PATH_INFO'], '/') : './';
+        if(!empty($user_info['network_id'])) {
+            $user_info['network_id'] = $user_info['network_id'];
+        } else {
+            $user_info['network_id'] = 'unknown';
+        }
+        if(isset($_SERVER['PATH_INFO'])) {
+            $current_path_info = ltrim($_SERVER['PATH_INFO'], '/');
+        }else {
+            $current_path_info = './';
+        }
         $this->nav_info['current_page_info']['logged_in_user'] = "{$this->fullname}";
         $this->benchmark->mark('get_user_details_end');
 
         $this->page_data = array();
         $this->page_data['navData'] = $this->nav_info;
-        $this->page_data['infoData'] = array('current_credentials' => $this->user_id, 'full_name' => $this->fullname);
+        $this->page_data['infoData'] = array(
+            'current_credentials' => $this->user_id,
+            'full_name' => $this->fullname
+        );
         $this->page_data['username'] = $this->username;
         $this->page_data['fullname'] = $this->fullname;
-        $this->page_data['load_prototype'] = false;
-        $this->page_data['load_jquery'] = true;
+        $this->page_data['load_prototype'] = FALSE;
+        $this->page_data['load_jquery'] = TRUE;
         $this->controller_name = $this->uri->rsegment(1);
     }
 }
