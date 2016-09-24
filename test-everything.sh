b1a071ade54d975b06546ebeab9e314091feda8e
dmlb2000 <dmlb2000@gmail.com>
2016-09-24 16:44:51 -0700

    only check the config and controllers subdirs for now...

--- test-everything.sh
+++ test-everything.sh
@@ -2,4 +2,4 @@
 
 # basic syntax check against all php files
 find application -name '*.php' | xargs -n 1 php -l
-find application -name '*.php' | xargs phpcs --standard=pacifica_php_ruleset.xml
+find application/config application/controllers -name '*.php' | xargs phpcs --standard=pacifica_php_ruleset.xml
