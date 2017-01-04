67efa57bcd895d221ac5abc606c992d5df0611f3
dmlb2000 <dmlb2000@gmail.com>
2017-01-04 10:52:20 -0800

    got some initial java script variables passed to status page

--- application/controllers/Status_api.php
+++ application/controllers/Status_api.php
@@ -152,7 +152,10 @@ class Status_api extends Baseline_api_controller
                 }
             }
             krsort($proposal_list);
-            $js = "var email_address = '{$this->email}';
+            $js = "var initial_proposal_id = '{$proposal_id}';
+                    var initial_instrument_id = '{$instrument_id}';
+                    var initial_time_period = '{$time_period}';
+                    var email_address = '{$this->email}';
                     var lookup_type = 't';
                     var initial_instrument_list = [];";
 
