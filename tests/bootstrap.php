2d2f9c60681371bf4f4b552158ea05d8daa98272
dmlb2000 <dmlb2000@gmail.com>
2017-01-31 21:32:24 -0800

    yet another define

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 define('BASEPATH', getcwd().'/system/');
 define('APPPATH', 'application/');
+define('VIEWPATH', 'application/views/');
 define('ENVIRONMENT', 'development');
 require_once BASEPATH.'core/CodeIgniter.php';
 require __DIR__ . '/../vendor/autoload.php';
