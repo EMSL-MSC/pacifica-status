87926764a433a619df8d952e66893c9657093992
dmlb2000 <dmlb2000@gmail.com>
2017-01-31 17:27:33 -0800

    imore travis scripts

--- travis/test-script.sh
+++ travis/test-script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -xe
 
+#!/bin/bash -xe
+
+if [ "$RUN_LINTS" = "true" ] ; then
+  bash -xe ./travis/static-analysis.sh
+else
+  bash -xe ./travis/unit-tests.sh
+  bash -xe ./travis/test-deploy.sh
+fi
+
 # basic syntax check against all php files
 find application -name '*.php' | xargs -n 1 php -l
 phpcs -n --extensions=php --ignore=*/websystem/*,*/system/*,*/migrations/*,*/libraries/*,*/logs/*,*/third_party/* --standard=pacifica_php_ruleset.xml application/
