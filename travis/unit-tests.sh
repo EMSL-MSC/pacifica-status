cc54a0ca7f85d5e1fa7c4c170112f786a94203f7
dmlb2000 <dmlb2000@gmail.com>
2017-02-01 09:08:26 -0800

    don't need this here

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -11,6 +11,5 @@ done
 docker run -it --rm --net=pacificauploadstatus_default -e METADATA_URL=http://metadataserver:8121 -e PYTHONPATH=/usr/src/app pacifica/metadata python test_files/loadit.py
 docker-compose stop uploadstatus
 echo "doing unit tests"
-phpenv config-add travis/coverage.ini
 cp vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumCommon/phpunit_coverage.php .
 ./vendor/bin/phpunit --coverage-text tests
