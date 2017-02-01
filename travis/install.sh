b55c2c3b4f6599ab8bba5f59b99a1608b2bf212d
dmlb2000 <dmlb2000@gmail.com>
2017-02-01 09:26:08 -0800

    add php-fpm configuration

--- travis/install.sh
+++ travis/install.sh
@@ -2,4 +2,20 @@
 composer update --no-interaction --no-ansi --no-progress --no-suggest --optimize-autoloader --prefer-stable
 phpenv config-add travis/coverage.ini
 PHP_FPM_BIN="$HOME/.phpenv/versions/$PHP_VERSION/sbin/php-fpm"
-
+DIR=$(realpath $(dirname "$0"))
+USER=$(whoami)
+PHP_VERSION=$(phpenv version-name)
+ROOT=$(realpath "$DIR/..")
+PORT=9000
+SERVER="/tmp/php.sock"
+function tmpl {
+    sed \
+        -e "s|{DIR}|$DIR|g" \
+        -e "s|{USER}|$USER|g" \
+        -e "s|{PHP_VERSION}|$PHP_VERSION|g" \
+        -e "s|{ROOT}|$ROOT|g" \
+        -e "s|{PORT}|$PORT|g" \
+        -e "s|{SERVER}|$SERVER|g" \
+        < $1 > $2
+}
+tmpl travis/php-fpm.tmpl.conf travis/php-fpm.conf
