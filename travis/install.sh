29e557643e674605dcd9d8b27af4925ed2dcb53e
dmlb2000 <dmlb2000@gmail.com>
2017-02-01 09:07:45 -0800

    move some stuff to install.sh script since it's more complicated now

---
+++ travis/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -xe
+travis_retry composer update --no-interaction --no-ansi --no-progress --no-suggest --optimize-autoloader --prefer-stable
+phpenv config-add travis/coverage.ini
+PHP_FPM_BIN="$HOME/.phpenv/versions/$PHP_VERSION/sbin/php-fpm"
+
