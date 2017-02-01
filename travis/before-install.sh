f4b82f9c660a7b9e8fb914911b4563f3267481b2
dmlb2000 <dmlb2000@gmail.com>
2017-01-31 22:52:31 -0800

    stop mysql as well

--- travis/before-install.sh
+++ travis/before-install.sh
@@ -7,4 +7,4 @@ sudo rm -f /usr/local/bin/docker-compose
 sudo curl -L -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)
 sudo chmod +x /usr/local/bin/docker-compose
 sudo service postgresql stop
-
+sudo service mysql stop
