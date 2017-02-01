a161fe7ec4e41adb5e8db7a44eef161e12d17c6e
dmlb2000 <dmlb2000@gmail.com>
2017-01-31 22:24:55 -0800

    try to do some docker-compose magic

---
+++ travis/before-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+composer self-update
+composer clear-cache
+
+sudo rm -f /usr/local/bin/docker-compose
+sudo curl -L -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)
+sudo chmod +x /usr/local/bin/docker-compose
+sudo service postgresql stop
+
+if [ -z "$RUN_LINTS" ] ; then
+  
+fi
