817243bd77b022bada3dc82df7af29349d67c174
dmlb2000 <dmlb2000@gmail.com>
2017-01-31 21:22:34 -0800

    try to make this bootstrap work in travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-define('BASEPATH', getcwd());
+define('BASEPATH', getcwd().'/system/core');
 define('APPPATH', 'application');
 require_once BASEPATH.'core/CodeIgniter.php';
 require __DIR__ . '/../vendor/autoload.php';
