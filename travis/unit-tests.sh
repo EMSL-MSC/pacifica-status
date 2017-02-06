e80656bc6f621f8f9722ad4ce524bfd8c6ab1c6c
David Brown <dmlb2000@gmail.com>
2017-02-05 20:52:00 -0800

    Pfft in my scripting I lost the username password

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -13,9 +13,9 @@ docker-compose stop uploadstatus
 echo "doing unit tests"
 cp vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumCommon/phpunit_coverage.php .
 ./vendor/bin/phpunit --coverage-text tests
-HTTP_CODE=$(curl -sL -w "%{http_code}\\n" localhost:8192/status_api/overview -o /dev/null || true)
+HTTP_CODE=$(curl -sL -w "%{http_code}\\n" -u dmlb2001:1234 localhost:8192/status_api/overview -o /dev/null || true)
 if [[ $HTTP_CODE != 200 ]] ; then
-  curl localhost:8192/status_api/overview
+  curl -u dmlb2001:1234 localhost:8192/status_api/overview
   cat travis/error.log || true
   cat travis/php-error.log || true
   cat travis/php-fpm.conf
