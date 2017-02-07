45eae77927deb2ace228e65b48d9fc8630d62f4a
David Brown <dmlb2000@gmail.com>
2017-02-06 19:37:59 -0800

    try dumping the selenium log

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -13,6 +13,7 @@ docker-compose stop uploadstatus
 echo "doing unit tests"
 cp vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumCommon/phpunit_coverage.php .
 ./vendor/bin/phpunit --coverage-text tests
+cat /tmp/selenium-server.log
 HTTP_CODE=$(curl -sL -w "%{http_code}\\n" -u dmlb2001:1234 localhost:8192/status_api/overview -o /dev/null || true)
 if [[ $HTTP_CODE != 200 ]] ; then
   curl -u dmlb2001:1234 localhost:8192/status_api/overview
