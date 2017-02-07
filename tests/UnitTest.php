7cbd3b3546759f8f382a42bec928e624477943af
David Brown <dmlb2000@gmail.com>
2017-02-06 19:15:01 -0800

    forgot the basic auth bits

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -6,12 +6,12 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase
     protected function setUp()
     {
         $this->setBrowser('firefox');
-        $this->setBrowserUrl('http://localhost:8192/status_api/overview');
+        $this->setBrowserUrl('http://dmlb2001:1234@localhost:8192/status_api/overview');
     }
 
     public function testTitle()
     {
-        $this->url('http://localhost:8192/status_api/overview');
+        $this->url('http://dmlb2001:1234@localhost:8192/status_api/overview');
         $this->assertEquals('Example WWW Page', $this->title());
     }
 }
