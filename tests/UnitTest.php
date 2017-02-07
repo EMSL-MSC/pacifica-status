9bebddcd1066c48d0df7cf6d27652bac8eaa09de
David Brown <dmlb2000@gmail.com>
2017-02-06 22:27:36 -0800

    try changing this back to the 2TestCase

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -1,19 +1,17 @@
 <?php
-require_once 'vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumTestCase.php';
-
-class WebTest extends PHPUnit_Extensions_SeleniumTestCase
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected $coverageScriptUrl = 'http://localhost:8193/';
 
     protected function setUp()
     {
-        $this->setBrowser('*firefox');
+        $this->setBrowser('firefox');
         $this->setBrowserUrl('http://dmlb2001:1234@localhost:8192/status_api/overview');
     }
 
     public function testTitle()
     {
-        $this->open('http://dmlb2001:1234@localhost:8192/status_api/overview');
+        $this->url('http://dmlb2001:1234@localhost:8192/status_api/overview');
         $this->assertEquals('MyEMSL Status - Overview', $this->title());
     }
 }
