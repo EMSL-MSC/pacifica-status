472d2372540a1e37ee32ffb59d3fb4046ff7790c
dmlb2000 <dmlb2000@gmail.com>
2017-04-19 15:41:17 -0700

    clean this up a bit

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -16,8 +16,8 @@ set -x
 docker run -it --rm --net=pacificauploadstatus_default -e METADATA_URL=http://metadataserver:8121 -e PYTHONPATH=/usr/src/app pacifica/metadata python test_files/loadit.py
 docker-compose stop uploadstatus
 ln -s application/resources project_resources
-mkdir -p application/reports/tmp
 if ! ./vendor/bin/phpunit --verbose --coverage-text tests ; then
+  ls index.php*
   cat /tmp/selenium-server.log || true
   cat travis/error.log || true
   cat travis/access.log || true
@@ -25,7 +25,6 @@ if ! ./vendor/bin/phpunit --verbose --coverage-text tests ; then
   cat travis/php-access.log || true
   exit -1
 fi
-ls -l application/reports/tmp
 cat travis/access.log
 cat travis/error.log || true
 cat travis/php-error.log || true
