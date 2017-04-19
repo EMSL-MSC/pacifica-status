a3703b3da8e3921cf575b4a8f7cee17c277d2001
dmlb2000 <dmlb2000@gmail.com>
2017-04-19 15:40:31 -0700

    this should clean things up

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -11,9 +11,6 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 
     public function testTitle()
     {
-        $session = $this->prepareSession();
-        $session->cookie()->remove('PHPUNIT_SELENIUM_TEST_ID');
-        $session->cookie()->add('PHPUNIT_SELENIUM_TEST_ID', 'WebTest__testTitle')->set();
         $this->url('http://localhost:8192/status_api/overview');
         $this->assertEquals('MyEMSL Status - Overview', $this->title());
     }
