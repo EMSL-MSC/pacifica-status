099e50b972566013c3a933bf53f26dee6cff0f8f
Ken Auberry <kenneth.auberry@pnnl.gov>
2017-10-27 15:01:16 -0700

    More path changes

--- test-everything.sh
+++ test-everything.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 # basic syntax check against all php files
-./vendor/bin/phpcs -n --extensions=php --ignore=*/websystem/*,*/third_party/*,*/system/*,*/migrations/*,*/libraries/*,*/logs/* --standard=myemsl_ruleset.xml application/
+./vendor/bin/phpcs -n --extensions=php --ignore=*/websystem/*,*/third_party/*,*/system/*,*/migrations/*,*/libraries/*,*/logs/* --standard=./myemsl_ruleset.xml application/
 if [[ $CODECLIMATE_REPO_TOKEN ]]; then
 phpunit --coverage-clover build/logs/clover.xml tests
 ./vendor/bin/test-reporter
