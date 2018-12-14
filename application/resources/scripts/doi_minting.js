35955365763c61950548882599e6698bf871a89e
Ken Auberry <kauberry@mac.com>
2018-12-14 11:37:58 -0800

    Cleaning up some link copying behaviors

--- application/resources/scripts/doi_minting.js
+++ application/resources/scripts/doi_minting.js
@@ -19,40 +19,6 @@ var setup_doi_linking_button = function(el) {
     return doi_linking_button;
 };
 
-var setup_doi_reference_copy_button = function(el, doi_reference) {
-    var transaction_id = el.find(".transaction_identifier").val();
-    var doi_link = format_doi_ref(doi_reference);
-    var doi_reference_copy_button = el.find(".doi_reference_button");
-    if(!doi_reference_copy_button.length){
-        doi_reference_copy_button = $("<button>", {
-            "class": "doi_linking_button",
-            "style": "z-index: 4;",
-            "id": "doi_reference_button_" + transaction_id,
-            "alt": "Copy DOI reference link to clipboard",
-            "title": "Copy DOI reference link to clipboard",
-            "name": "doi_reference_button_" + transaction_id,
-            "data-clipboard-text": doi_link,
-            "data-clipboard-action": "copy",
-            "text": "Copy DOI Reference Link"
-        });
-    }
-    return doi_reference_copy_button;
-};
-
-var setup_doi_copied_notification = function(el) {
-    var copied_notification = el.find(".copied_notification");
-    var transaction_id = el.find(".transaction_identifier").val();
-    if(!copied_notification.length){
-        copied_notification = $("<button>", {
-            "class": "copied_notification",
-            "id": "copied_notification_" + transaction_id,
-            "name": "copied_notification_" + transaction_id,
-            "text": "Link Copied!",
-            "style": "margin-right: 6px; display:none; transition: none;"
-        });
-    }
-    return copied_notification;
-};
 var setup_doi_staging_button = function(el) {
     var transaction_id = el.find(".transaction_identifier").val();
     var doi_staging_button = el.find(".doi_staging_button");
@@ -73,12 +39,12 @@ var setup_doi_staging_button = function(el) {
         });
 
         doi_staging_button_container.append(doi_staging_button);
-
-        var doi_linking_button = setup_doi_linking_button(el);
-        doi_staging_button_container.append(doi_linking_button);
-
-        var copied_notification = setup_doi_copied_notification(el);
-        doi_staging_button_container.append(copied_notification);
+        doi_staging_button_container = add_link_copy_info(
+            el,
+            doi_staging_button_container,
+            setup_doi_linking_button,
+            el.find(".site_url_identifier")
+        );
         doi_staging_button.on("click", function(event){
             create_doi_data_resource($(event.target));
         });
@@ -87,10 +53,6 @@ var setup_doi_staging_button = function(el) {
     if(!doi_staging_button.is(":visible")){
         doi_staging_button.fadeIn("slow");
     }
-};
-
-var format_doi_ref = function(doi_reference){
-    return "https://dx.doi.org/" + doi_reference;
 };
 
 var create_doi_data_resource = function(el) {
@@ -179,7 +141,7 @@ var publish_released_data = function(el, form_data) {
                             }
                         })
                             .done (
-                                function(data){
+                                function(){
                                     setup_staging_buttons();
                                     window.open(doi_ui_base + "registrations/" + regID);
                                 }
@@ -302,90 +264,4 @@ $(function(){
 
         }
     });
-    var clipboard = new ClipboardJS(".doi_linking_button");
-    clipboard.on("success", function(e) {
-        var notif = $(e.trigger).parent("div").find(".copied_notification");
-        notif.fadeIn().delay(500).fadeOut();
-    });
 });
-
-var set_release_state_banners = function(release_states, selector){
-    $(selector).each(function(index, el){
-        el = $(el);
-        var txn_id = el.find(".transaction_identifier").val();
-        var ribbon_el = el.find(".ribbon");
-        var release_info = release_states[txn_id];
-        var transaction_id = release_info.transaction;
-        var doi_release_state = "";
-        if(release_info.release_state == "released"){
-            //add doi staging button
-            doi_release_state = "released";
-            doi_display_state = "Released";
-            el.find(".upload_url").attr({"href": external_release_base_url + "released_data/" + txn_id});
-            el.find(".release_date").val(release_info.release_date);
-            // var pub_status_block = el.next(".publication_status_block");
-            if(release_info.release_doi_entries && release_info.release_doi_entries.length > 0){
-                item = release_info.release_doi_entries[0];
-                doi_reference = item.doi_reference;
-                if (item.doi_status == "saved") {
-                    doi_release_state = "doi_pending";
-                    doi_display_state = "DOI Pending";
-                    link_text = "Submitted (Pending Release)";
-                    link = doi_ui_base + "registrations/" + item.metadata.minting_api_id;
-                } else if (item.doi_status == "pending") {
-                    doi_release_state = "doi_pending";
-                    doi_display_state = "DOI Pending";
-                    link_text = "Awaiting Approval at Datacite";
-                } else {
-                    doi_release_state = "minted";
-                    doi_display_state = "DOI Minted";
-                    link = format_doi_ref(item.doi_reference);
-                    link_text = item.doi_reference;
-                }
-            }
-
-            if (typeof setup_doi_staging_button === "function") {
-                if (doi_release_state == "released"){
-                    setup_doi_staging_button(el, transaction_id);
-                }else{
-                    var doi_staging_button_container = el.find(".doi_staging_button_container");
-                    if(!doi_staging_button_container.length){
-                        doi_staging_button_container = $("<div/>", {
-                            "class": "staging_buttons buttons"
-                        });
-                    }
-                    var doi_linking_button = setup_doi_reference_copy_button(el, doi_reference);
-                    doi_staging_button_container.append(doi_linking_button);
-                    var doi_copied_notification = setup_doi_copied_notification(el);
-                    doi_staging_button_container.append(doi_copied_notification);
-                    el.find("legend").after(doi_staging_button_container);
-                    if(!doi_staging_button_container.is(":visible")){
-                        doi_staging_button_container.fadeIn("slow");
-                    }
-                }
-
-            }
-        }else{
-            var current_session_contents = JSON.parse(sessionStorage.getItem("staged_releases"));
-            if(!$.isEmptyObject(current_session_contents) && txn_id in current_session_contents){
-                release_info.release_state = "staged";
-                release_info.display_state = "Staged";
-            }else{
-                release_info.release_state = "not_released";
-                release_info.display_state = "Not Released";
-                var content = build_staging_button(txn_id);
-                el.find("legend").after(content);
-                el.find(".staging_button").off().on("click", function(event){
-                    stage_transaction($(event.target));
-                });
-            }
-            doi_release_state = release_info.release_state;
-            doi_display_state = release_info.display_state;
-        }
-        el.find(".release_state").next("td.metadata_item").text(doi_release_state);
-        el.find(".release_state_display").next("td.metadata_item").text(doi_display_state);
-        ribbon_el.removeClass().addClass("ribbon").addClass(doi_release_state);
-        ribbon_el.find("span").text(doi_display_state);
-
-    });
-};
